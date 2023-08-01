941740809e833eead644ae8ad048cdf4c3d062dd
Jhoan Zamora <jhoan.zamora@wizeline.com>
2023-08-01 09:53:34 -0500

    Images

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.texts}></div>
+      <Image src={Hero} alt="" className={styles.img} />
+    </div>
+  );
 }
